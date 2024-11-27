6bba63a4b79011c407b3035ad60eb31d5043c89e
Takuma Ohsawa <s23c1025wd@s.chibakoudai.jp>
2024-11-27 12:00:34 +0900

    Add a command

---
+++ soinsu.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+
+def prime_factors(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+        if divisor * divisor > n and n > 1:
+            factors.append(n)
+            break
+    return factors
+
+if len(sys.argv) > 2:
+    print("エラー: 引数は一つだけ指定してください。")
+else:
+    try:
+        if len(sys.argv) == 2:
+
+            num = int(sys.argv[1])
+        else:
+
+            num = int(input("自然数を入力してください: "))
+
+        if num < 2:
+            print("2以上の自然数を入力してください。")
+        else:
+
+            factors = prime_factors(num)
+            print(f"{num} の素因数分解: {' × '.join(map(str, factors))}")
+    except ValueError:
+        print("無効な入力です。数値を入力してください。")
+
